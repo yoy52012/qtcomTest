1a59215982d8238c42aa248b91e3d4898f27f8a2
yuanchao <476987156@qq.com>
2016-05-24 08:42:21 +0800

    初始化qtcom测试程序版本库

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <QtGlobal>
+#if QT_VERSION >= 0x050000
+#include <QApplication>
+#else
+#include <QtGui/QApplication>
+#endif
+#include "mainwindow.h"
+#include <QTextCodec>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+#if QT_VERSION < 0x050000
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("utf8"));
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("utf8")); // 关键是这句
+#endif
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
